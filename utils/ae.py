4ea3e1562831944afb8d4159a96c01d5cb955d57
Pulin Agrawal <pulinagrawal@users.noreply.github.com>
2017-03-01 14:17:19 -0600

    working with sessioning

--- utils/ae.py
+++ utils/ae.py
@@ -5,8 +5,6 @@ import tensorflow as tf
 from functools import wraps
 
 tf.set_random_seed(0)
-import numpy as np
-inp = np.random.random_sample([100,784])
 
 
 class Autoencoder(object):
@@ -16,9 +14,10 @@ class Autoencoder(object):
     Also capable of applying sparse autoencoder with a boolean parameter.
 
     """
-    def __init__(self, _network_architecture, session=tf.Session(), learning_rate=0.001,
+
+    def __init__(self, network_architecture, session=tf.Session(), learning_rate=0.001,
                  batch_size=100, sparse=False, sparsity=0.1, transfer_fct=tf.nn.sigmoid, tied_weights=True):
-        self._network_architecture = _network_architecture
+        self._network_architecture = network_architecture
         self.learning_rate = learning_rate
         self.batch_size = batch_size
         self.sparse = sparse
@@ -26,10 +25,13 @@ class Autoencoder(object):
         self._sess = session
         self._transfer_fct = transfer_fct
         self._tied_weights = tied_weights
+        self._new = True
+        self.weights = None
+        self.biases = None
 
         # tf Graph input
-        self._x = tf.placeholder(tf.float32, [self.batch_size, _network_architecture[0]])
-        print(self.batch_size, 'x',  _network_architecture[0])
+        self._x = tf.placeholder(tf.float32, [self.batch_size, network_architecture[0]])
+        print(self.batch_size, 'x',  network_architecture[0])
 
         # Create autoencoder network
         self._layers = []
@@ -96,13 +98,13 @@ class Autoencoder(object):
         print("Network")
         prev_layer_output = self._x
         layers = [dict(zip(['weights', 'biases'], _layer))
-                      for _layer in zip(self._network_weights, self._network_biases)]
+                  for _layer in zip(self._network_weights, self._network_biases)]
         for i, layer in enumerate(layers):
             current_layer = self._transfer_fct(tf.matmul(prev_layer_output, layer['weights'])+layer['biases'])
             self._layers.append(current_layer)
             if i == len(self._network_architecture)-1:
                 self._encoding_layer = current_layer
-            print(prev_layer_output,'x',layer['weights'],'+',layer['biases'])
+            print(prev_layer_output, 'x', layer['weights'], '+', layer['biases'])
             prev_layer_output = current_layer
 
     def _KL_divergence(self, units):
@@ -140,14 +142,26 @@ class Autoencoder(object):
         def sessioned_func(self, *args, **kwargs):
             if self._sess is None:
                 self._sess = tf.Session()
-                if self.weights is not None:
-                    set_weights = tf.assign(self._network_weights, self.weights)
-                    set_biases = tf.assign(self._network_biases, self.biases)
-                    self._sess.run([set_weights, set_biases])
-                else:
+            if self._new:
+                if self.weights is None:
                     self._sess.run(tf.global_variables_initializer())
+                    self._new = False
+                else:
+                    set_weights = list()
+                    set_biases = list()
+                    weights_zip = zip(self._network_weights[:len(self._network_weights)//2], self.weights)
+                    for weight_tensor, weight in weights_zip:
+                        set_weights.append(tf.assign(weight_tensor, weight))
+                    for biases_tensor, biases in zip(self._network_biases, self.biases):
+                        set_biases.append(tf.assign(biases_tensor, biases))
+                    self._sess.run([set_weights, set_biases])
+                    self._new = False
             return session_func(self, *args, **kwargs)
         return sessioned_func
+
+    @_sessioned
+    def setup(self):
+        pass
 
     @_sessioned
     def partial_fit(self, input_batch):
@@ -170,13 +184,37 @@ class Autoencoder(object):
         """ Use Autoencoder to reconstruct given data. """
         return self._sess.run(self._layers[-1], feed_dict={self._x: input_tensor})
 
-    @classmethod
-    def save(cls, instance, filename):
+    def save(self, filename):
+        save_list = [{'network_architecture': self._network_architecture,
+                      'learning_rate': self.learning_rate,
+                      'batch_size': self.batch_size,
+                      'sparse': self.sparse,
+                      'sparsity': self.rho,
+                      'transfer_fct': self._transfer_fct,
+                      'tied_weights': self._tied_weights},
+                     self.weights[:len(self.weights)//2],
+                     self.biases]
+
         with open(filename, 'wb') as save_file:
-            pickle.dump(instance, save_file)
+            pickle.dump(save_list, save_file)
 
     @classmethod
     def load_model(cls, filename):
         with open(filename, 'rb') as load_file:
-            instance = pickle.load(load_file)
+            load_list = pickle.load(load_file)
+            instance = Autoencoder(**load_list[0])
+            instance.weights = load_list[1]
+            instance.biases = load_list[2]
+            instance.setup()
         return instance
+
+
+sae = Autoencoder([784,500,200],sparse=True)
+import numpy as np
+inp = np.random.random_sample([100,784])
+print(sae.partial_fit(inp))
+print(sae.partial_fit(inp))
+sae.save('test_save1.ae')
+new_sae = Autoencoder.load_model('test_save1.ae')
+print(new_sae.partial_fit(inp))
+
