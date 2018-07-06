50857e1488d4aea680c2289e28e482bb4567f939
Pulin Agrawal <pulinagrawal@users.noreply.github.com>
2018-07-06 14:18:57 -0500

    refactor utility methods out to analysis package

--- src/analysis/enrichment.py
+++ src/analysis/enrichment.py
@@ -1,78 +1,15 @@
-from src.utils import ae, stats, reverse_dict_of_lists, get_gsm_labels
 from src.utils.stats import hypgeom_mean, hypgeom_var, hypgeom_pmf
+from src.utils import ae, stats, reverse_dict_of_lists, get_gsm_labels
 from collections import defaultdict
 from collections import Counter
 from functools import partial
 from bisect import bisect
+from src.analysis import setup_analysis, build_genesetlist_from_units, get_activations
 import numpy as np
 import os
 import tqdm
-import re
-import pickle as pkl
-from functools import lru_cache
 
-
-@lru_cache(maxsize=10)
-def get_indexed_label_hashmap(dataset, epochs):
-    return dict(zip(dataset.labels.tolist(), range(len(dataset.labels))))
-
-
-def get_input(gsm, datasets):
-    for dataset in datasets:
-        labels_map = get_indexed_label_hashmap(dataset, dataset.epochs_completed)
-        try:
-            data = dataset.images[labels_map[gsm]]
-            return data
-        except KeyError:
-            print('GSM not found in any datasets')
-    return None
-
-
-def get_result_file_dict(biology_result_file):
-    biology = defaultdict(list)
-    with open(biology_result_file) as f:
-        for line in f:
-            if 'unit' in line:
-                if '_gsa' in line:
-                    unit = int(re.findall(r'\d+', line)[-1])
-                    while True:
-                        line = f.readline()
-                        geneset = line.split('\t')[0]
-                        if geneset != '\n' and geneset != '':
-                            biology[unit].append(geneset)
-                        if line == '\n' or line == '':
-                            break
-    return biology
-
-
-def print_unit_biology(unit_num, biology_result_file):
-    with open(biology_result_file) as f:
-        for line in f:
-            if 'unit' + str(unit_num) + '_gsa' in line:
-                print('unit ' + str(unit_num))
-                while True:
-                    line = f.readline()
-                    print(line, end='')
-                    if line == '\n' or line == '':
-                        break
-                break
-
-def get_activations(gsm, datasets, model):
-    input_genes = get_input(gsm, datasets)
-    if input_genes is not None:
-        model_input = np.expand_dims(input_genes, axis=0)
-        activations = model.encoding(model_input)[0]
-    return activations
-
-def build_genelist_from_units(units, geneset_unit_map, display=False, bio_result_file=''):
-    geneset_list = []
-    for unit in units:
-        geneset_list.extend(geneset_unit_map[unit])
-        if display and bio_result_file != '':
-            print_unit_biology(unit, bio_result_file)
-    return geneset_list
-
-def get_genesets(gsm, geneset_unit_map, model, datasets, for_top_x_pct_units=0.1, random=False, disp=False,
+def get_activated_genesets(gsm, geneset_unit_map, model, datasets, for_top_x_pct_units=0.1, random=False, disp=False,
                  bio_result_file=''):
     """Returns a list of genesets with their respective counts of the times
         they showed up in the model with the given biology_hash (a dictionary of units with
@@ -98,7 +35,7 @@ def get_genesets(gsm, geneset_unit_map, model, datasets, for_top_x_pct_units=0.1
         # added one to unit number because biology_results file assume units start with 1
 
     top_units = [unit + 1 for unit in top_units]
-    geneset_list = build_genelist_from_units(top_units, geneset_unit_map, disp, bio_result_file)
+    geneset_list = build_genesetlist_from_units(top_units, geneset_unit_map, disp, bio_result_file)
 
     return list(set(geneset_list))
 
@@ -106,7 +43,7 @@ def get_genesets(gsm, geneset_unit_map, model, datasets, for_top_x_pct_units=0.1
 def get_ggec(gsm_list, geneset_unit_map, model, datasets, for_top_x_pct_units):
     ggec = Counter()
     for gsm in gsm_list:
-        ggec += Counter(get_genesets(gsm, geneset_unit_map, model, datasets, for_top_x_pct_units))
+        ggec += Counter(get_activated_genesets(gsm, geneset_unit_map, model, datasets, for_top_x_pct_units))
     return ggec
 
 
@@ -220,7 +157,7 @@ def enrichment(gsm_list, model, geneset_unit_map, for_top_x_pct_units, datasets,
         # 2a - pick pre-determined number of random gsms
         _, random_gsm_list = datasets[0].next_batch(len(gsm_list))
         for gsm in random_gsm_list:
-            test_biology += Counter(get_genesets(gsm, geneset_unit_map, model,
+            test_biology += Counter(get_activated_genesets(gsm, geneset_unit_map, model,
                                                  datasets,
                                                  for_top_x_pct_units=for_top_x_pct_units)
                                     )
@@ -234,7 +171,7 @@ def enrichment(gsm_list, model, geneset_unit_map, for_top_x_pct_units, datasets,
     gsm_list_biology = []
     for gsm in gsm_list:
         # collect all the genesets enriched in a GSM for all GSMs
-        gsm_list_biology.extend(get_genesets(gsm, geneset_unit_map, model, datasets,
+        gsm_list_biology.extend(get_activated_genesets(gsm, geneset_unit_map, model, datasets,
                                              for_top_x_pct_units=for_top_x_pct_units,
                                              disp=display_units_for_gsms,
                                              bio_result_file=bio_result_file))
@@ -300,21 +237,18 @@ def count_greater(arr, k):
 def main():
     model_name = 'geodb_ae_89.net'
     model_folder = os.path.join('results', 'best_attmpt_2')
-    model_file = os.path.join(model_folder, model_name)
-    model = ae.Autoencoder.load_model(model_file, logdir=os.path.join('results', 'features_' + model_name))
+
+    labelled_data_files = ['GSE15061_aml.txt', 'GSE15061_mds.txt']
 
     result_filename = 'best_sparse_biology.txt'
-    print("using "+result_filename)
-    bio_result_file = os.path.join(model_folder, result_filename)
-    for_top_x_pct_units = 0.02
 
     normed_split_path = os.path.join('data', 'normd_split_')
     split = 1
-    unlabelled, labelled, validation, test = pkl.load(open(normed_split_path + str(split) + '.pkl', 'rb'))
 
-    labelled_data_files = ['GSE15061_aml.txt', 'GSE15061_mds.txt']
-    gsm_labels = extract_labels(labelled_data_files, [labelled, unlabelled])
-    geneset_unit_map = get_result_file_dict(bio_result_file)
+    for_top_x_pct_units = 0.02
+
+    dataset, geneset_unit_map, gsm_labels, model = setup_analysis(labelled_data_files, model_folder, model_name,
+                                                               normed_split_path, result_filename, split)
 
     '''
     _ = unlabelled.next_batch(unlabelled.num_examples)
@@ -322,14 +256,14 @@ def main():
     _, gsm_labels[1] = unlabelled.next_batch(len(gsm_labels[1]))
     labelled_data_files = ['Random1', 'Random2']
     '''
-    comparision_dict_0 = enrichment_ref(gsm_labels[0], model, geneset_unit_map, for_top_x_pct_units, [unlabelled, validation])
+    comparision_dict_0 = enrichment_ref(gsm_labels[0], model, geneset_unit_map, for_top_x_pct_units, dataset)
     print(labelled_data_files[0]+" genesets")
     print_comp_dict(comparision_dict_0)
     set1 = get_really_enriched(comparision_dict_0)
 
     print("")
 
-    comparision_dict_1 = enrichment_ref(gsm_labels[1], model, geneset_unit_map, for_top_x_pct_units, [unlabelled, validation])
+    comparision_dict_1 = enrichment_ref(gsm_labels[1], model, geneset_unit_map, for_top_x_pct_units, dataset)
     print(labelled_data_files[1]+" genesets")
     print_comp_dict(comparision_dict_1)
     set2 = get_really_enriched(comparision_dict_1)
@@ -361,20 +295,6 @@ def main():
                 gene = geneset
                 fdr = filename_dict[filename_set][2][geneset]['fdr']
                 writer.writerow([geneset, fdr])
-
-
-def extract_labels(labelled_data_files, datasets):
-    labelled_data_folder = os.path.join('data')
-    # labelled_data_files = ['GSE8052_asthma_1.txt', 'GSE8052_asthma_0.txt']
-    # labelled_data_files = ['GDS4602_3.txt', 'GDS4602_4.txt']
-    gsm_labels = get_gsm_labels(labelled_data_files, labelled_data_folder)
-    for class_id in gsm_labels:
-        print("size before:", len(gsm_labels[class_id]))
-        for i, gsm in enumerate(gsm_labels[class_id]):
-            if get_input(gsm, datasets=datasets) is None:
-                gsm_labels[class_id].pop(i)
-        print("size after:", len(gsm_labels[class_id]))
-    return gsm_labels
 
 
 sort_comp_dict = lambda y, by: sorted(y, key=lambda x: (y[x][by], x))
